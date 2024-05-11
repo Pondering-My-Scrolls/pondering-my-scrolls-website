1c43711f3d2ce01907a991c9782e890e9bb72c9f
Nicky <nickywolfe3@gmail.com>
2024-05-11 16:02:53 -0500

    Create date component

---
+++ components/postPreviewDate.js
@@ -0,0 +1,8 @@
+// https://date-fns.org/v2.16.1/docs/format
+
+import { parseISO, format } from 'date-fns';
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLL dd / yy')}</time>;
+}
