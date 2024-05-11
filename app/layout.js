5384371d4b1610a5207357ad307f5fcd441af774
JenicaWoitowicz <74977775+JenicaWoitowicz@users.noreply.github.com>
2024-05-11 14:22:23 -0500

    Footer revamp

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,11 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 
 import Header from "../components/header/Header.js";
-import Footer from "../components/footer.js";
+import Footer from "../components/footer/footer.js";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  
+
   return (
     <html lang="en">
       <head />
