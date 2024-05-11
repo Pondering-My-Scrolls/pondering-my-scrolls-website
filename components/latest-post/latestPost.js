0e607b6a74b0ecd8d890312044655f76356acca1
Nicky <nickywolfe3@gmail.com>
2024-05-11 16:35:23 -0500

    Create latest post component

--- components/latest-post/latestPost.js
+++ components/latest-post/latestPost.js
@@ -4,13 +4,21 @@ import Link from "next/link";
 import './latest-post.scss';
 
 function latestPost(props) {
+  const truncate = (input) => input?.length > 35 ? `${input.substring(0, 35)}...` : input;
+  
   return (
     <article className="latest-post">
       <h2>
+        Our latest post
+      </h2>
+      <h3>
         <Link href={`/posts/${props.slug}`}>
           {props.title}
         </Link>
-      </h2>
+      </h3>
+      <p>
+        {truncate(props.summary)}
+      </p>
     </article>
   )
 }
