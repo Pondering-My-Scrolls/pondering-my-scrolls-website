9ff02779e699c9e7b163b6bac54f03d95fe58c59
Nicky <nickywolfe3@gmail.com>
2024-05-11 19:58:57 -0500

    Set up accessibility page

---
+++ app/accessibility/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import PageHeader from '../../components/page-header/pageHeader.js';
+
+export const metadata = {
+    title: 'Accessibility | Pondering My Scrolls',
+}
+
+function Accessibility() {
+    return (
+        <main className="wo">
+            <PageHeader h1="Accessibility" />
+            <div className="wysiwyg">
+                <p>
+                    This website has been created in a way in which it intends to comply wih WCAG version 2.2 Level AAA accessibility standards.
+                </p>
+                <p>
+                    It's our responsibility as good internet neighbours to fix any accessibility issues as soon as possible. Please email Nicky at hello@nicky.dev with any accessibility concerns or feature requests.
+                </p>
+            </div>
+        </main>
+    )
+}
+
+export default Accessibility
