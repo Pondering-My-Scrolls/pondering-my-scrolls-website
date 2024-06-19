d98006d0228f880c6cd57708846a04df1cf8c53c
Nicky <nickywolfe3@gmail.com>
2024-06-18 19:53:24 -0500

    ~further destruction~ and !creation!

--- components/PostMetadata.ts
+++
@@ -1,8 +0,0 @@
-export interface PostMetadata {
-    title: string;
-    date: string;
-    slug: string;
-    author: string;
-    category: string;
-    summary: string;
-}
--- components/postDate.js
+++
@@ -1,8 +0,0 @@
-// https://date-fns.org/v2.16.1/docs/format
-
-import { parseISO, format } from 'date-fns';
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>This {format(date, 'Do')} day of {format(date, 'MMMM, yyy')}</time>;
-}
