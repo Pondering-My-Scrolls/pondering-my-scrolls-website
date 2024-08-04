7303e556b3d1e10b779d264c33918d3208f9af0a
Nicky <nickywolfe3@gmail.com>
2024-08-04 10:11:20 -0500

    Refactor JS + add escape closure

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,41 +1,54 @@
-
-function siteNavToggle() {
-    let navButton = document.querySelector('#siteNavToggle');
+function toggleNavigation() {
     let navModal = document.querySelector('.header__nav__modal');
+    let navButton = document.querySelector('#siteNavToggle');
+    let state = navButton.getAttribute("aria-expanded");
 
-    navButton.addEventListener("click", function () {
-        let state = this.getAttribute("aria-expanded");
+    if (state == "true") {
+        state = "false"
+    } else {
+        state = "true"
+    }
 
-        if (state == "true") {
-            state = "false"
-        } else {
-            state = "true"
-        }
-
-        this.setAttribute("aria-expanded", state);
-        navModal.classList.toggle('open');
-    });
+    navButton.setAttribute("aria-expanded", state);
+    navModal.classList.toggle('open');
 }
 
 function summaryToggle() {
     let summaryButton = document.querySelector('#summaryToggle');
     let summary = document.querySelector('.summary');
 
-    summaryButton.addEventListener("click", function () {
-        let state = this.getAttribute("aria-expanded");
+    let state = summaryButton.getAttribute("aria-expanded");
 
-        if (state == "true") {
-            state = "false";
-        } else {
-            state = "true";
-        }
+    if (state == "true") {
+        state = "false";
+    } else {
+        state = "true";
+    }
 
-        this.setAttribute("aria-expanded", state);
-        summary.classList.toggle('open');
-    });
+    summaryButton.setAttribute("aria-expanded", state);
+    summary.classList.toggle('open');
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    siteNavToggle();
-    summaryToggle();
+    let navButton = document.querySelector('#siteNavToggle');
+    let summaryButton = document.querySelector('#summaryToggle');
+
+    navButton.addEventListener("click", function () {
+        toggleNavigation();
+    });
+
+    addEventListener("keyup", (event) => {
+        let navButton = document.querySelector('#siteNavToggle');
+        let state = navButton.getAttribute("aria-expanded");
+
+        if (state == "true" && event.key == "Escape") {
+            toggleNavigation();
+        }
+    });
+
+    if (summaryButton) {
+        summaryButton.addEventListener("click", function () {
+            summaryToggle()
+        });
+    }
 });
